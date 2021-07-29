7f335026ddaeecc1b42390ee687df205761a2875
AaronKwong929 <AaronKwong929@gmail.com>
2021-07-29 16:03:03 +0800

    321

---
+++ src/components/VirtualList.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    ref="scrollingContainer"
+    class="virtual-list"
+    :style="{ height: `${height}px` }"
+    @scroll="handleScroll"
+  >
+    <div
+      ref="phantomContainer"
+      :style="{ height: `${phantomHeight}px`, position: `relative` }"
+    >
+      {{  }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `VirtualList`,
+
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+
+    height: {
+      type: Number,
+      required: true,
+    },
+
+    total: {
+      type: Number,
+      required: true,
+    },
+
+    estimateRowHeight: {
+      type: Number,
+      required: true,
+    },
+
+    buffer: {
+      type: Number,
+      default: 5,
+    },
+  },
+
+  computed: {
+    phantomHeight({ estimateRowHeight, total }) {
+      return estimateRowHeight * total;
+    },
+
+    limit({ height, estimateRowHeight }) {
+      return Math.ceil(height / estimateRowHeight);
+    },
+  },
+
+  watch: {},
+
+  methods: {
+    handleScroll() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.virtual-list {
+  overflow-y: auto;
+  overflow-x: hidden;
+  position: relative;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <virtual-list
+      :buffer="bufferSize"
+      :list="list"
+      :total="total"
+      :height="800"
+      :estimate-row-height="50"
+    />
+  </div>
+</template>
+
+<script>
+// import faker from 'faker';
+import VirtualList from '@/components/VirtualList.vue';
+
+export default {
+  name: `Home`,
+
+  components: {
+    VirtualList,
+  },
+
+  data() {
+    return {
+      bufferSize: 5,
+      list: [],
+    };
+  },
+
+  computed: {
+    total({ list }) {
+      return list.length;
+    },
+  },
+
+  mounted() {
+    for (let i = 0; i < 10000; i++) {
+      // this.data.push(faker.loream.sentences());
+      this.data.push(`${i}${i}${i}${i}${i}`);
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100vh;
+}
+</style>
