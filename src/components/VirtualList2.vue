585a0dff85fd8f809cce62daa384b21e2af085e2
AaronKwong929 <AaronKwong929@gmail.com>
2021-07-30 18:05:28 +0800

    fefefefefefefe

--- src/components/VirtualList2.vue
+++ src/components/VirtualList2.vue
@@ -19,7 +19,7 @@
       class="actual-content"
       :style="{
         position: `absolute`,
-        transForm: getTransform(),
+        transform: getTransform(),
         top: 0,
         width: `100%`,
       }"
@@ -28,10 +28,7 @@
         v-for="item in renderContentList"
         :key="`list-item-${item.index}`"
         :id="`list-item-${item.index}`"
-        :style="{
-          width: `100%`,
-          height: `${estimateRowHeight}px`,
-        }"
+        style="margin-bottom: 5px; border: 1px solid black;"
       >
         {{ item.content }}
       </div>
@@ -59,11 +56,6 @@ export default {
       required: true,
     },
 
-    // estimateRowHeight: {
-    //   type: Number,
-    //   required: true,
-    // },
-
     buffer: {
       type: Number,
       default: 5,
@@ -79,7 +71,7 @@ export default {
 
       cachedPositions: [],
       phantomHeight: 0,
-      estimateRowHeight: 0,
+      estimateRowHeight: 50,
     };
   },
 
@@ -94,33 +86,25 @@ export default {
   },
 
   watch: {
-    list() {
-      if (this.$refs[`actualContent`] && this.total) {
-        this.updateCachedPositions();
-      }
-    },
+    list() {},
   },
 
   methods: {
     handleScroll(event) {
       if (event.target === this.$refs[`scrollingContainer`]) {
-        const {
-          target: { scrollTop },
-        } = event;
-
-        const currentStartIndex = Math.floor(
-          scrollTop / this.estimateRowHeight
-        );
-        if (currentStartIndex !== this.startIndex) {
+        const { scrollTop } = event.target;
+        // console.log(`aaaa`, this.originalStartIndex);
+        const currentStartIndex = this.getStartIndex(scrollTop);
+        console.log(`current Start Index`, this.currentStartIndex);
+        if (currentStartIndex !== this.originalStartIndex) {
           this.originalStartIndex = currentStartIndex;
-
+          // console.log(this.originalStartIndex, currentStartIndex, this.buffer);
           this.startIndex = Math.max(this.originalStartIndex - this.buffer, 0);
-          console.log(`start index`, this.startIndex);
+          // console.log(`start index`, this.startIndex);
           this.endIndex = Math.min(
-            currentStartIndex + this.limit,
+            this.originalStartIndex + this.limit + this.buffer,
             this.total - 1
           );
-
           this.scrollTop = scrollTop;
         }
       }
@@ -138,17 +122,17 @@ export default {
       const { estimateRowHeight, list } = this;
       this.cachedPositions = [];
       for (let i = 0; i < list.length; i++) {
-        this.cachedPositions.push({
+        this.cachedPositions[i] = {
           index: i,
           height: estimateRowHeight,
           top: i * estimateRowHeight,
           bottom: (i + 1) * estimateRowHeight,
           dValue: 0,
-        });
+        };
       }
-      this.estimateRowHeight = this.cachedPositions[
-        this.cachedPositions.length - 1
-      ].bottom; // NOTE: 总高度等于数组最后一个元素的 bottom 数值
+      // this.estimateRowHeight = this.cachedPositions[
+      //   this.cachedPositions.length - 1
+      // ].bottom; // NOTE: 总高度等于数组最后一个元素的 bottom 数值
     },
 
     updateCachedPositions() {
@@ -156,7 +140,8 @@ export default {
       const start = nodeList[0];
       nodeList.forEach(node => {
         if (!node) return;
-        const { height } = node.getBoundingClientRect();
+        const rect = node.getBoundingClientRect();
+        const { height } = rect;
         const index = Number(node.id.split(`-`)[2]);
         const oldHeight = this.cachedPositions[index].height;
         const dValue = oldHeight - height;
@@ -168,16 +153,14 @@ export default {
         }
       });
 
-      // eslint-disable-next-line no-unused-vars
       let startIndex = 0;
       if (start) startIndex = Number(start.id.split(`-`)[2]);
       const { length: cachedPositionsLen } = this.cachedPositions;
       let cumulativeDiffHeight = this.cachedPositions[startIndex].dValue;
       this.cachedPositions[startIndex].dValue = 0;
 
-      for (let i = startIndex + 1; i < cachedPositionsLen; ++i) {
+      for (let i = startIndex + 1; i < cachedPositionsLen; i++) {
         const item = this.cachedPositions[i];
-        // update height
         this.cachedPositions[i].top = this.cachedPositions[i - 1].bottom;
         this.cachedPositions[i].bottom =
           this.cachedPositions[i].bottom - cumulativeDiffHeight;
@@ -193,16 +176,30 @@ export default {
       this.phantomHeight = height;
       // this.$refs[`phantomContainer`].style.height = `${height}px`;
     },
-  },
 
-  updated() {},
+    // NOTE: 可改二分提高效率
+    getStartIndex(scrollTop = 0) {
+      for (let i = 0; i < this.cachedPositions.length; i++) {
+        const target = this.cachedPositions[i];
+        if (target.bottom > scrollTop) {
+          return target.index;
+        }
+      }
+    },
+  },
 
   mounted() {
     this.endIndex = Math.min(
       this.startIndex + this.limit + this.buffer,
       this.total - 1
     );
+    this.phantomHeight = this.total * this.estimateRowHeight;
+
     this.initCachedPositions();
+
+    if (this.$refs[`actualContent`] && this.total) {
+      this.updateCachedPositions();
+    }
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <virtual-list
+    <!-- <virtual-list
       v-if="list && list.length > 0"
       :buffer="bufferSize"
       :list="list"
       :total="total"
       :height="800"
       :estimate-row-height="100"
+    /> -->
+    <virtual-list
+      v-if="list && list.length > 0"
+      :buffer="bufferSize"
+      :list="list"
+      :total="total"
+      :height="600"
     />
   </div>
 </template>
 
 <script>
-// import faker from 'faker';
+import faker from 'faker';
 import VirtualList from '@/components/VirtualList2.vue';
 
 export default {
@@ -39,7 +46,7 @@ export default {
     const list = [];
     for (let i = 0; i < 10000; i++) {
       // this.data.push(faker.loream.sentences());
-      list.push({ index: i, content: i });
+      list.push({ index: i, content: faker.lorem.sentences() });
     }
     this.list = list;
   },
